fa4646f688cb25faa65dd433a7354c5b81f295d5
karlaR87 <101289949+karlaR87@users.noreply.github.com>
2022-06-17 23:20:21 -0600

    Add files via upload

---
+++ Muebles_SV-Bassedata.sql
@@ -0,0 +1,47 @@
+DROP DATABASE if EXISTS Mueble_SV;
+CREATE DATABASE Mueble_SV;
+USE Mueble_SV;
+
+CREATE TABLE Usuarios(
+id_user INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+user_name VARCHAR (30) NOT NULL,
+contraseña VARCHAR (30) NOT NULL,
+tipo_de_usuario int (30) NOT NULL
+);
+
+INSERT INTO Usuarios(user_name, contraseña, tipo_de_usuario) VALUES("Admin", "Admin123", 0); 
+INSERT INTO Usuarios(user_name, contraseña, tipo_de_usuario) VALUES("Trabajador", "Trabajador123", 1);
+
+SELECT * FROM Usuarios;
+
+DESCRIBE Usuarios;
+
+CREATE TABLE Materiales_Secundario(
+Nombre_de_MaterialSe VARCHAR (30) NOT NULL,
+ID_Material_Se VARCHAR (30) PRIMARY KEY NOT NULL,
+Precio_MateriaSe varchar (30) NOT NULL
+);
+
+
+CREATE TABLE Materiales_prim(
+Nombre_de_MaterialPr VARCHAR (30) NOT NULL,
+ID_Material_Pr VARCHAR (30) PRIMARY KEY NOT NULL,
+Precio_MaterialpR varchar (30) NOT NULL
+);
+
+
+
+CREATE TABLE Catalogo(
+Nombre_de_producto VARCHAR (30) NOT NULL,
+ID_producto VARCHAR (30) PRIMARY KEY NOT NULL,
+ID_Material_Pr VARCHAR (30),
+CONSTRAINT FK_IDMaterialPr
+FOREIGN KEY (ID_Material_Pr) 
+REFERENCES Materiales_prim(ID_Material_Pr),
+ID_Material_Se VARCHAR (30),
+CONSTRAINT FK_IDMaterialSe
+FOREIGN KEY (ID_Material_Se) 
+REFERENCES Materiales_Secundario(ID_Material_Se),
+Precio_de_mano_de_obra_bruto float (30) NOT NULL
+);
+
